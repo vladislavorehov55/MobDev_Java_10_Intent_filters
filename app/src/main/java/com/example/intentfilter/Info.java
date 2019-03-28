50d9aa1c1996d7741e7453e22d44e29560bf234d
koroteevmv <koroteevmv@gmail.com>
2019-03-28 16:01:22 +0400

    Action for several intents added

--- app/src/main/java/com/example/intentfilter/Info.java
+++ app/src/main/java/com/example/intentfilter/Info.java
@@ -1,7 +1,12 @@
 package com.example.intentfilter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Info extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        // получаем Intent, который вызывал это Activity
+        Intent intent = getIntent();
+        // читаем из него action
+        String action = intent.getAction();
+
+        String format = "", textInfo = "";
+
+        // в зависимости от action заполняем переменные
+        if (action.equals("com.example.intent.action.showtime")) {
+            format = "HH:mm:ss";
+            textInfo = "Time: ";
+        }
+        else if (action.equals("com.example.intent.action.showdate")) {
+            format = "dd.MM.yyyy";
+            textInfo = "Date: ";
+        }
+
+        // в зависимости от содержимого переменной format
+        // получаем дату или время в переменную datetime
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String datetime = sdf.format(new Date(System.currentTimeMillis()));
+
+        TextView tvDate = (TextView) findViewById(R.id.tvInfo);
+        tvDate.setText(textInfo + datetime);
     }
+
 }
